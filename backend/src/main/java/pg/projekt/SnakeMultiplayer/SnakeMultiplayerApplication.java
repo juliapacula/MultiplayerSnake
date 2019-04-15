27d745611e9a1a372bfe3c6fa54da1fbd999b2ce
Julia Pacula <paculajulia@gmail.com>
2019-04-15 22:18:17 +0200

    Created /backend and /frontend modules for game and created appropriate config

---
+++ backend/src/main/java/pg/projekt/SnakeMultiplayer/SnakeMultiplayerApplication.java
@@ -0,0 +1,27 @@
+package pg.projekt.SnakeMultiplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class SnakeMultiplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeMultiplayerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/*").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
+
+}
--- src/main/resources/static/api/input.endpoint.js
+++
@@ -1,5 +0,0 @@
-const PREFIX = '/snake';
-const InputEndpoint = {
-  postPosition: PREFIX + '/position/post',
-  lastPosition: PREFIX +'/position/last',
-};
